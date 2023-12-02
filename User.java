6b34d0665b7bbac56984103b8d71eb3f397477a6
Anar Mammadli 17928 <amammadli17928@ada.edu.az>
2023-12-03 01:23:35 +0400

    added new method and fixed some errors

--- User.java
+++ User.java
@@ -12,29 +12,23 @@ public class User implements Serializable {
     private String username;
     private String password;
     
-    public User(String username, String password) {
-        try {
-            setUsername(username);
-        } catch (IllegalArgumentException | NullPointerException e) {
-            System.out.println("Invalid input detected in Username. Please provide valid credentials.");
-        }
-        try {
-            setPassword(password);
-        } catch (IllegalArgumentException | NullPointerException e) {
-            System.out.println("Invalid input detected in Password. Please provide valid credentials.");
-        }
+    public User(String username, String password) throws IllegalArgumentException, NullPointerException {
+        setUsername(username);
+        setPassword(password);
+    }
+    public User(){
     }
     public String getUsername() {
         return username;
     }
     public void setUsername(String username) {
-        if(username == null) throw new NullPointerException();
-        if(username.isBlank()) throw new IllegalArgumentException();
+        if(username == null) throw new NullPointerException("Username must contain only a-z/A-Z/0-9!");
+        if(username.isBlank()) throw new IllegalArgumentException("Username must be written!");
         for(int i = 0; i < username.length(); i++){
             char c = username.charAt(i);
             if((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z') || (c >= '0' && c <= '9')) continue;
             else{
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Username must contain only a-z/A-Z/0-9!");
             } 
         }
         this.username = username;
@@ -43,13 +37,14 @@ public class User implements Serializable {
         return password;
     }
     public void setPassword(String password) {
-        if(password == null) throw new NullPointerException();
-        if(password.isBlank()) throw new IllegalArgumentException();
+        if(password == null) throw new NullPointerException("Password must contain only a-z/A-Z/0-9!");
+        if(password.isBlank()) throw new IllegalArgumentException("Password must be written!");
+        if((password.length()<=7)) throw new IllegalArgumentException("Password must be at least 8 characters!");
         for(int i = 0; i < password.length(); i++){
             char c = password.charAt(i);
-            if((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z') || (c >= '0' && c <= '9')) continue;
+            if((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z')) continue;
             else{
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Password must contain only a-z/A-Z/0-9!");
             } 
         }
         this.password = password;
@@ -60,16 +55,27 @@ public class User implements Serializable {
     }
     
     public static void register(String username, String password) {
-        // Check if the username is already taken
-        if (userExists(username)) {
-            System.out.println("Username already exists. Please choose a different username.");
-            return;
+        try {
+            // Check if the username is already taken
+            if (userExists(username)) {
+                System.out.println("Username already exists. Please choose a different username.");
+                return;
+            }
+            // Attempt to create a new User object
+            User newUser = new User(username, password);
+    
+            // Load existing users, add the new user, and save the updated list
+            List<User> existingUsers = loadFromDatabase();
+            existingUsers.add(newUser);
+            saveToDatabase(existingUsers);
+    
+            System.out.println("Registration successful.");
+        } catch (IllegalArgumentException | NullPointerException e) {
+            // Handle the exception (print a message or log it)
+            System.out.println(e.getMessage());
         }
-        User newUser = new User(username, password);
-        // Save the user information to the database file
-        saveToDatabase(newUser);
-        System.out.println("Registration successful.");
     }
+    
 
     private static boolean userExists(String username) {
         // Check if the username exists in the database
@@ -82,19 +88,22 @@ public class User implements Serializable {
         return false;
     }
 
-    private static void saveToDatabase(User user) {
+    private static void saveToDatabase(List<User> users) {
         // Save user information to the database file
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("UserDatabase/user_database.txt", true))) {
-            oos.writeObject(user);
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("UserDatabase.txt"))) {
+            for (User user : users) {
+                oos.writeObject(user);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
 
-    private static List<User> loadFromDatabase() {
+    public static List<User> loadFromDatabase() {
         // Load user information from the database file
         List<User> users = new ArrayList<>();
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("UserDatabase/user_database.txt"))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("UserDatabase.txt"))) {
             while (true) {
                 try {
                     User user = (User) ois.readObject();
@@ -104,10 +113,20 @@ public class User implements Serializable {
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+
         }
         return users;
     }
+
+    public boolean checkLogin(String username, String password) {
+        List<User> users = User.loadFromDatabase();
+
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true; // Username and password match
+            }
+        }
+
+        return false; // No matching username and password found
+    }
 }
-
-
