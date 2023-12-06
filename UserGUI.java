405cb0030d36d91eb9fffd14ad543deee1ac15b1
Anar Mammadli 17928 <amammadli17928@ada.edu.az>
2023-12-07 00:29:55 +0400

    added

---
+++ UserGUI.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserGUI extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private User user;
+
+    public UserGUI() {
+        setTitle("User Authentication");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel usernameLabel = new JLabel("Username:");
+        panel.add(usernameLabel);
+
+        usernameField = new JTextField();
+        panel.add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        panel.add(passwordLabel);
+
+        passwordField = new JPasswordField();
+        panel.add(passwordField);
+
+        JButton registerButton = new JButton("Register");
+        panel.add(registerButton);
+
+        JButton loginButton = new JButton("Login");
+        panel.add(loginButton);
+
+        user = new User();
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+                User.register(username, password);
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+                boolean loggedIn = user.checkLogin(username, password);
+                if (loggedIn) {
+                    JOptionPane.showMessageDialog(null, "Login successful!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password");
+                }
+            }
+        });
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new UserGUI();
+            }
+        });
+    }
+}
